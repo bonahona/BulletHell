b24fb7832c8e0c7f7bc07e8c6ed127472d63f0f4
Bona <bjorn.fyrvall@gmail.com>
2018-03-10 22:06:09 +0100

    Added ui.

---
+++ Assets/Game/UI/ScoreManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : Manager<ScoreManager>
+{
+    public Text ScoreText;
+
+    private int CurrentScore;
+
+    public void AddScore(int score)
+    {
+        CurrentScore += score;
+        ScoreText.text = CurrentScore.ToString("D8");
+    }
+}
