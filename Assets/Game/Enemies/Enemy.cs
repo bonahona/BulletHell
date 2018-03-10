23f0741337afb84a77ec8e8388692cb234045244
Bona <bjorn.fyrvall@gmail.com>
2018-03-10 14:10:16 +0100

    Added override of animation.

--- Assets/Game/Enemies/Enemy.cs
+++ Assets/Game/Enemies/Enemy.cs
@@ -2,7 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Enemy: EntityBase
 {
+    public AnimationClip AnimationClip;
 
+    private Animator Animator;
+
+    private AnimatorOverrideController AnimatorOverride;
+
+    private void Start()
+    {
+        Animator = GetComponent<Animator>();
+        AnimatorOverride = new AnimatorOverrideController(Animator.runtimeAnimatorController);
+        Animator.runtimeAnimatorController = AnimatorOverride;
+
+        AnimatorOverride["CowAnimation"] = AnimationClip;
+    }
 }
